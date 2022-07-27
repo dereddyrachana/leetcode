3b275912d1602b6e6bd7bdc279685ad38a68ce63
Rachana <32927512+dereddyrachana@users.noreply.github.com>
2022-07-27 18:16:39 -0400

    Time: 90 ms, Memory: 14.6 MB - LeetHub

--- valid-anagram/valid-anagram.py
+++ valid-anagram/valid-anagram.py
@@ -1,3 +1,16 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return collections.Counter(s) == collections.Counter(t)
+        d1 = {}
+        d2 = {}
+        for char in s:
+            if char not in d1:
+                d1[char] = 1
+            else:
+                d1[char]+=1
+        for char in t:
+            if char not in d2:
+                d2[char] = 1
+            else:
+                d2[char]+=1
+        return d1 == d2
+            
