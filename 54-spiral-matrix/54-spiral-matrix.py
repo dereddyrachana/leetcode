75b300a975d574b3786f54bf885c9b880730a94c
Rachana <32927512+dereddyrachana@users.noreply.github.com>
2022-07-27 00:35:08 -0400

    Time: 35 ms (85.28%), Space: 13.9 MB (82.47%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -1,35 +1,32 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        result = []
-        rows, columns = len(matrix), len(matrix[0])
-        up = left = 0
-        right = columns - 1
-        down = rows - 1
+        row_begin = 0
+        col_begin = 0
+        row_end = len(matrix) - 1
+        col_end = len(matrix[0]) - 1
+        ans = []
+        while(row_begin <= row_end and col_begin <= col_end):
+            for i in range(col_begin,col_end+1):
+                # print(matrix[row_begin][i])
+                ans.append(matrix[row_begin][i])
+            row_begin+=1
+            for j in range(row_begin,row_end+1):
+                ans.append(matrix[j][col_end])
+                # print(matrix[j][col_end])
+            col_end-=1
+            if(row_begin <= row_end):
+                for i in range(col_end, col_begin-1,-1):
+                    # print(matrix[row_end][i])
+                    ans.append(matrix[row_end][i])
+                row_end-=1
+            if(col_begin <= col_end):
+                for i in range(row_end,row_begin-1,-1):
+                    # print(matrix[i][col_begin])
+                    ans.append(matrix[i][col_begin])
+                col_begin+=1
+        return ans
+            
 
-        while len(result) < rows * columns:
-            # Traverse from left to right.
-            for col in range(left, right + 1):
-                result.append(matrix[up][col])
-
-            # Traverse downwards.
-            for row in range(up + 1, down + 1):
-                result.append(matrix[row][right])
-
-            # Make sure we are now on a different row.
-            if up != down:
-                # Traverse from right to left.
-                for col in range(right - 1, left - 1, -1):
-                    result.append(matrix[down][col])
-
-            # Make sure we are now on a different column.
-            if left != right:
-                # Traverse upwards.
-                for row in range(down - 1, up, -1):
-                    result.append(matrix[row][left])
-
-            left += 1
-            right -= 1
-            up += 1
-            down -= 1
-
-        return result
+        
+            
+            
