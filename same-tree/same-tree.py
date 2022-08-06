3116f423a27ac617d669150f0e53209089204ba9
Rachana <32927512+dereddyrachana@users.noreply.github.com>
2022-08-05 22:08:13 -0400

    Time: 75 ms, Memory: 13.8 MB - LeetHub

--- same-tree/same-tree.py
+++ same-tree/same-tree.py
@@ -5,12 +5,24 @@ class Solution:
         :type q: TreeNode
         :rtype: bool
         """    
-        # p and q are both None
         if not p and not q:
             return True
-        # one of p and q is None
         if not q or not p:
             return False
         if p.val != q.val:
             return False
-        return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
+        leftsubTree = self.isSameTree(p.left,q.left)
+        rightsubTree = self.isSameTree(p.right,q.right)
+        return leftsubTree and rightsubTree
+    
+    
+#         return True
+        # # p and q are both None
+        # if not p and not q:
+        #     return True
+        # # one of p and q is None
+        # if not q or not p:
+        #     return False
+        # if p.val != q.val:
+        #     return False
+        # return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
