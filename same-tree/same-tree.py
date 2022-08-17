bca43903a05d14c459b6b5958a7a9f3334a8938e
Rachana <32927512+dereddyrachana@users.noreply.github.com>
2022-08-17 13:54:58 -0400

    Time: 54 ms, Memory: 13.9 MB - LeetHub

--- same-tree/same-tree.py
+++ same-tree/same-tree.py
@@ -1,28 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def isSameTree(self, p, q):
-        """
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: bool
-        """    
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if not p and not q:
             return True
-        if not q or not p:
+        if p and not q:
             return False
-        if p.val != q.val:
+        if q and not p:
             return False
-        leftsubTree = self.isSameTree(p.left,q.left)
-        rightsubTree = self.isSameTree(p.right,q.right)
-        return leftsubTree and rightsubTree
-    
-    
-#         return True
-        # # p and q are both None
-        # if not p and not q:
-        #     return True
-        # # one of p and q is None
-        # if not q or not p:
-        #     return False
-        # if p.val != q.val:
-        #     return False
-        # return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
+        if p.val!=q.val:
+            return False
+        left = self.isSameTree(p.left,q.left)
+        right = self.isSameTree(p.right,q.right)
+        
+        return left and right
+
