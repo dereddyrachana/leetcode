64208132566de97e5ea466b8b10fc0b1e1280c57
Rachana <32927512+dereddyrachana@users.noreply.github.com>
2022-08-18 02:20:02 -0400

    Time: 102 ms (96.30%), Space: 17.3 MB (7.87%) - LeetHub

---
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
@@ -0,0 +1,27 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        adj = {i:[] for i in range(n)}
+        connected = 0
+        for n1, n2 in edges:
+            adj[n1].append(n2)
+            adj[n2].append(n1)
+        
+        visited = set()
+        def dfs(i):
+            if i not in visited:
+                visited.add(i)
+                for j in adj[i]:
+                    dfs(j)
+            
+        for i in range(0,n):
+            if i not in visited:
+                connected+=1
+                dfs(i)
+            
+        
+        return connected
+            
+            
+            
+                    
+            
