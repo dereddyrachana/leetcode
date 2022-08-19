ebcc435d5be862435987e79807edb1f0fd8bfe6e
Rachana <32927512+dereddyrachana@users.noreply.github.com>
2022-08-19 16:42:18 -0400

    Time: 43 ms (85.18%), Space: 13.9 MB (32.84%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -3,17 +3,35 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# class Solution:
+#     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+#         if l1 is None:
+#             return l2
+#         elif l2 is None:
+#             return l1
+#         elif l1.val < l2.val:
+#             l1.next = self.mergeTwoLists(l1.next,l2)
+#             return l1
+#         else:
+#             l2.next = self.mergeTwoLists(l1,l2.next)
+#             return l2
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        if l1 is None:
+        if not l1:
             return l2
-        elif l2 is None:
+        if not l2:
             return l1
-        elif l1.val < l2.val:
+        if l1.val < l2.val:
             l1.next = self.mergeTwoLists(l1.next,l2)
             return l1
         else:
-            l2.next = self.mergeTwoLists(l1,l2.next)
+            l2.next = self.mergeTwoLists(l1, l2.next)
             return l2
-    
+        
+
         
