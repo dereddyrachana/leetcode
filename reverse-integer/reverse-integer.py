a15b6a5c35f07d1e86b13e166dd8c4f8b0177d9d
Rachana <32927512+dereddyrachana@users.noreply.github.com>
2022-08-19 05:09:00 -0400

    Time: 58 ms, Memory: 13.8 MB - LeetHub

--- reverse-integer/reverse-integer.py
+++ reverse-integer/reverse-integer.py
@@ -1,13 +1,12 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        sign = 1
         x = str(x)
+        sign = 1
         if x[0] == '-':
             x = x[1:]
             sign = -1
         x = x[::-1]
         print(x)
-        if int(x) >= 2**31 -1:
+        if int(x) >= 2**31:
             return 0
         return sign * int(x)
-        
