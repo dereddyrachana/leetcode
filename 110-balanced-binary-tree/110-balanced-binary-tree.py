f245bd35697af601e7c11676f005f6e3b056f29a
Rachana <32927512+dereddyrachana@users.noreply.github.com>
2022-08-20 09:33:49 -0400

    Time: 64 ms (80.25%), Space: 18.6 MB (61.23%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.py
+++ 110-balanced-binary-tree/110-balanced-binary-tree.py
@@ -1,19 +1,10 @@
-class Solution:
-    # Compute the tree's height via recursion
-    def height(self, root: TreeNode) -> int:
-        # An empty tree has height -1
-        if not root:
+class Solution(object):
+    def isBalanced(self, root):
+        return (self.Height(root) >= 0)
+    def Height(self, root):
+        if root is None:  
+            return 0
+        leftheight, rightheight = self.Height(root.left), self.Height(root.right)
+        if leftheight < 0 or rightheight < 0 or abs(leftheight - rightheight) > 1:  
             return -1
-        return 1 + max(self.height(root.left), self.height(root.right))
-    
-    def isBalanced(self, root: TreeNode) -> bool:
-        # An empty tree satisfies the definition of a balanced tree
-        if not root:
-            return True
-
-        # Check if subtrees have height within 1. If they do, check if the
-        # subtrees are balanced
-        return abs(self.height(root.left) - self.height(root.right)) < 2 \
-            and self.isBalanced(root.left) \
-            and self.isBalanced(root.right)
-        
+        return max(leftheight, rightheight) + 1
