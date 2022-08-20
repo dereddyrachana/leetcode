50bc4455959cb2b3a47a814c2f4eae87ac2943ac
Rachana <32927512+dereddyrachana@users.noreply.github.com>
2022-08-20 07:09:51 -0400

    Time: 2070 ms (15.20%), Space: 25.1 MB (38.55%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -12,3 +12,25 @@ class Solution:
             right+=1
         return max_price
         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # left = 0
+        # right = 1
+        # max_price = 0
+        # while(right < len(prices)):
+        #     curr_price = prices[right] - prices[left]
+        #     if(prices[left] < prices[right]):
+        #         max_price = max(curr_price,max_price)
+        #     else:
+        #         left = right
+        #     right+=1
+        # return max_price
+        
