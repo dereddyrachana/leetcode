b23f48d2d70c589fedb9a19d0d29c13c04aad836
Rachana <32927512+dereddyrachana@users.noreply.github.com>
2022-08-24 11:57:21 -0400

    Time: 55 ms (45.97%), Space: 14.2 MB (35.80%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,12 +1,23 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        final = []
         
-        def dfs(curr,nums,res):
-            res.append(curr)
+        def backtrack(cur, nums, res):
+            final.append(cur)
             for i in range(len(nums)):
-                dfs(curr+[nums[i]], nums[i+1:], res)
+                backtrack(cur+[nums[i]],nums[i+1:],res)
+        backtrack([],nums,0)
+        return final
+    
+#     class Solution:
+#     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         res = []
+        
+#         def dfs(curr,nums,res):
+#             res.append(curr)
+#             for i in range(len(nums)):
+#                 dfs(curr+[nums[i]], nums[i+1:], res)
                 
-        dfs([],nums,res)
-        return res
+#         dfs([],nums,res)
+#         return res
             
