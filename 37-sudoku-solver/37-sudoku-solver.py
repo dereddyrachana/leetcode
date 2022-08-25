fc68ce004f92310eb4916453e23bb22b2b98fa8f
Rachana <32927512+dereddyrachana@users.noreply.github.com>
2022-08-25 06:40:23 -0400

    Time: 1130 ms (15.77%), Space: 14 MB (42.79%) - LeetHub

---
+++ 37-sudoku-solver/37-sudoku-solver.py
@@ -0,0 +1,30 @@
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        d = ["1","2","3","4","5","6","7","8","9"]
+        def solve(board):
+            for i in range(len(board)):
+                for j in range(len(board[0])):
+                    if board[i][j]==".":
+                        for c in range(len(d)):
+                            if isValid(i,j,d[c]):
+                                board[i][j]=d[c]
+                                if solve(board)==True:
+                                    return True
+                                else:
+                                    board[i][j]="."                                    
+                        return False
+            return True
+        
+        def isValid(row,col,c):
+            for i in range(9):
+                if board[i][col]==c:
+                    return False
+                
+                if board[row][i]==c:
+                    return False
+                
+                if board[3*(row//3)+(i//3)][3*(col//3)+(i%3)]==c:
+                    return False
+            return True
+        solve(board)
+        
